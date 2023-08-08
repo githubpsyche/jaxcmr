15fa1b58f46231ad817a4d3806ef69821bc7476c
githubpsyche <gunnjordanb@gmail.com>
2023-08-07 22:06:24 -0500

    simpler interface for LinearAssociativeMemory.__init__

--- jaxcmr/memory/LinearAssociativeMemory/__init__.py
+++ jaxcmr/memory/LinearAssociativeMemory/__init__.py
@@ -1,13 +1,3 @@
-from jaxcmr.memory.LinearAssociativeMemory.LinearAssociativeMemory import LinearAssociativeMemory, associate
-
-from jaxcmr.memory.LinearAssociativeMemory.Mfc import LinearAssociativeMfc, init_linear_mfc
-from jaxcmr.memory.LinearAssociativeMemory.Mcf import LinearAssociativeMcf, init_linear_mcf
-
-__all__ = [
-    "LinearAssociativeMemory",
-    "associate",
-    "LinearAssociativeMfc",
-    "init_linear_mfc",
-    "LinearAssociativeMcf",
-    "init_linear_mcf",
-]
+from jaxcmr.memory.LinearAssociativeMemory.LinearAssociativeMemory import *
+from jaxcmr.memory.LinearAssociativeMemory.Mfc import *
+from jaxcmr.memory.LinearAssociativeMemory.Mcf import *
