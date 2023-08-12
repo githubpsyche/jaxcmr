0887918b855a05110444ede18eea2f5d10adb9f4
githubpsyche <gunnjordanb@gmail.com>
2023-08-12 00:29:40 -0500

    black formatting for context.py

--- jaxcmr/context/Context.py
+++ jaxcmr/context/Context.py
@@ -14,9 +14,11 @@ __all__ = [
     "integrate_delay_context",
 ]
 
+
 class Context(Pytree):
     start_context_input: Float[Array, "context_feature_units"]
     delay_context_input: Float[Array, "context_feature_units"]
+
 
 @jit
 @dispatch.abstract
@@ -27,14 +29,20 @@ def integrate(
 ) -> Context:
     "Integrate an input representation into current state of a context representation"
 
-@jit
-@dispatch
-def integrate_start_context(context: Context, drift_rate: float | Float[Array, ""]) -> Context:
-    "Integrate start-of-list context into current state of a temporal context representation"
-    return integrate(context, context.start_context_input, drift_rate)
 
 @jit
 @dispatch
-def integrate_delay_context(context: Context, drift_rate: float | Float[Array, ""]) -> Context:
+def integrate_start_context(
+    context: Context, drift_rate: float | Float[Array, ""]
+) -> Context:
+    "Integrate start-of-list context into current state of a temporal context representation"
+    return integrate(context, context.start_context_input, drift_rate)
+
+
+@jit
+@dispatch
+def integrate_delay_context(
+    context: Context, drift_rate: float | Float[Array, ""]
+) -> Context:
     "Integrate out-of-list context into current state of a temporal context representation"
     return integrate(context, context.delay_context_input, drift_rate)
