dfd99977853003d595a001494f57e92c6765d3e0
Jordan Gunn <gunnjordanb@gmail.com>
2023-09-04 13:49:51 -0500

    refactor data simulation into shared simulate.py

--- jaxcmr/memorysearch/simulate.py
+++ jaxcmr/memorysearch/simulate.py
@@ -21,15 +21,22 @@ from jaxcmr.helpers import (
     ScalarInteger,
     Integer,
     Array,
+    Bool,
     study_events,
     recall_events,
-    trial_count
+    trial_count,
+    get_item_count,
+    select_parameters_by_subject,
+    tree_transpose,
+    recall_by_study_position
 )
-from jax import jit, random, lax
+import numpy as  np
+import numpy.matlib
+from jax import jit, random, lax, vmap, numpy as jnp
 
 # %% Exports
 
-__all__ = ["simulate_trial", "free_recall", "simulate_trials"]
+__all__ = ["free_recall", "simulate_trial", "simulate_h5_from_h5"]
 
 # %% Single event simulation
 
@@ -73,7 +80,7 @@ def free_recall(
 @partial(jit, static_argnums=(0, 1))
 @dispatch
 def simulate_trial(
-    model_create_fn: Callable,
+    model_create_fn: Callable[[ScalarInteger, ScalarInteger, dict], MemorySearch],
     item_count: ScalarInteger,
     presentation: Integer[Array, "study_events"],
     rng: PRNGKeyArray,
@@ -88,7 +95,7 @@ def simulate_trial(
 @partial(jit, static_argnums=(0, 1))
 @dispatch
 def simulate_trial(
-    model_create_fn: Callable,
+    model_create_fn: Callable[[ScalarInteger, ScalarInteger, dict], MemorySearch],
     item_count: ScalarInteger,
     rng: PRNGKeyArray,
     parameters: dict,
@@ -102,7 +109,7 @@ def simulate_trial(
 @partial(jit, static_argnums=(0, 1, 2))
 @dispatch
 def simulate_trials(
-    model_create_fn: Callable,
+    model_create_fn: Callable[[ScalarInteger, ScalarInteger, dict], MemorySearch],
     item_count: ScalarInteger,
     experiment_count: ScalarInteger,
     rng: PRNGKeyArray,
@@ -117,20 +124,59 @@ def simulate_trials(
     )
 
 
-@partial(jit, static_argnums=(0, 1, 2))
-@dispatch
-def simulate_trials(
-    model_create_fn: Callable,
-    item_count: ScalarInteger,
-    experiment_count: ScalarInteger,
-    presentation: Integer[Array, "study_events"],
+# %% Full dataset simulation
+
+
+def preallocate_for_h5_dataset(
+    data: dict, trial_mask: Bool[Array, "trial_count"], experiment_count: ScalarInteger
+):
+    """Pre-allocate a dictionary of arrays for a given trial mask."""
+    return {
+        key: np.matlib.repmat(
+            np.zeros(np.shape(data["pres_itemnos"][trial_mask]), dtype=np.int64),
+            experiment_count,
+            1,
+        )
+        if key == "recalls"
+        else np.matlib.repmat(data[key][trial_mask], experiment_count, 1)
+        for key in data
+    }
+
+
+def simulate_h5_from_h5(
+    model_create_fn: Callable[[ScalarInteger, ScalarInteger, dict], MemorySearch],
+    data: dict,
+    parameters: list[dict],
     rng: PRNGKeyArray,
-    parameters: dict,
-) -> Integer[Array, "trial_count recall_events"]:
-    """Initialize model and study events, then simulate and predict retrieval events"""
-    model = model_create_fn(item_count, presentation.shape[0], parameters)
-    model = start_retrieving(experience(model, presentation))
-    return lax.map(
-        lambda split_rng: free_recall(model, split_rng)[1],
-        random.split(rng, experiment_count),
+    trial_mask: Bool[Array, "trial_count"],
+    experiment_count: ScalarInteger,
+):
+    """Simulate a dataset from a dataset and specified model constructor and parameters."""
+    sim_h5 = preallocate_for_h5_dataset(data, trial_mask, experiment_count)
+    presentations = np.matlib.repmat(
+        data["pres_itemnos"][trial_mask], experiment_count, 1
     )
+    subject_indices = np.matlib.repmat(data["subject"][trial_mask], experiment_count, 1)
+    item_counts = vmap(get_item_count)(presentations)
+    _simulate_trials = vmap(simulate_trial, in_axes=(None, None, 0, 0, 0))
+
+    for item_count in jnp.unique(item_counts):
+        rng, _ = random.split(rng)
+        ic_mask = item_counts == item_count
+        ic_presentations = presentations[ic_mask]
+        ic_parameters = select_parameters_by_subject(
+            subject_indices[ic_mask], parameters
+        )
+
+        result = _simulate_trials(
+            model_create_fn,
+            item_count.item(),
+            ic_presentations,
+            random.split(rng, len(ic_presentations)),
+            tree_transpose(ic_parameters),
+        )
+        sim_h5["recalls"][ic_mask, : result.shape[1]] = vmap(recall_by_study_position)(
+            ic_presentations, result
+        )
+
+    return sim_h5
