6a2b2192eac233fdde884e58604b443b0ed409e1
Jordan Gunn <gunnjordanb@gmail.com>
2025-03-20 07:07:18 -0500

    Add demo script for CMRT analysis and simulation

---
+++ projects/cmrt/accessibility.py
@@ -0,0 +1,263 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %%
+#!%load_ext autoreload
+#!%autoreload 2
+
+# %%
+# | parametrization
+
+import json
+
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from jax import jit, lax, vmap
+from tqdm import trange
+
+from cmrt.cmr import CMRFactory as model_factory
+from cmrt.fitting import make_subject_trial_masks
+from cmrt.helpers import generate_trial_mask, load_data
+from cmrt.likelihood import MemorySearchLikelihoodFnGenerator as LikelihoodFnGenerator
+
+# %%
+# | CONFIG / PARAMETERS
+
+fit_path = "fits/Murdock1962_BaseCMR_full.json"
+data_path = "data/Murdock1962.h5"
+data_query = "data['subject'] > -1"
+
+use_progress_bar = True
+
+# %%
+# | CONFIG / PARAMETERS
+
+fit_path = "fits/HerrKaha24_BaseCMR_full.json"
+data_path = "data/HerrKaha24.h5"
+data_query = "data['subject'] > -1"
+
+use_progress_bar = True
+
+# %%
+# | LOAD FIT RESULTS & DATA
+
+with open(fit_path) as f:
+    fit_result = json.load(f)
+    if "subject" not in fit_result["fits"]:
+        fit_result["fits"]["subject"] = fit_result["subject"]
+
+
+data = load_data(data_path)
+trial_mask = generate_trial_mask(data, data_query)
+max_size = np.max(data["pres_itemnos"])
+connections = jnp.zeros((max_size, max_size))
+
+likelihood_generator = LikelihoodFnGenerator(model_factory, data, connections)
+
+subjects = data["subject"].flatten()
+subject_trial_masks, unique_subjects = make_subject_trial_masks(trial_mask, subjects)
+subject_range = (
+    trange(len(unique_subjects)) if use_progress_bar else range(len(unique_subjects))
+)
+
+# count how many unique list lengths there are per subject
+list_lengths = np.array(
+    [len(np.unique(data["listLength"][data["subject"] == s])) for s in unique_subjects]
+)
+list_lengths
+
+# %%
+# | helper functions
+
+
+@jit
+def get_accessibility(trial_index, parameters):
+    _, prescaled_activations = lax.scan(
+        lambda m, c: (
+            m.retrieve(c),
+            jnp.dot(m.context.state, m.mcf.state) * m.recallable,
+        ),
+        likelihood_generator.init_model_for_retrieval(trial_index, parameters),
+        likelihood_generator.trials[trial_index],
+    )
+
+    # converted to an average activation over recallable items at each retrieval attempt
+    # return jnp.sum(prescaled_activations, axis=1) / jnp.sum(
+    # prescaled_activations != 0, axis=1
+    # )
+
+    # converted to a maximum activation over recallable items at each retrieval attempt
+    # return jnp.max(prescaled_activations, axis=1)
+
+    # converted to a sum of activations over recallable items at each retrieval attempt
+    return jnp.sum(prescaled_activations, axis=1)
+
+
+@jit
+def get_termination_index(trial_index):
+    return jnp.sum(likelihood_generator.trials[trial_index] != 0)
+
+
+@jit
+def get_recall_positions(trial_index):
+    return jnp.arange(1, len(likelihood_generator.trials[trial_index]) + 1)
+
+
+@jit
+def get_list_length(trial_index):
+    return (likelihood_generator.present_lists[trial_index] != 0).sum()
+
+
+@jit
+def get_reaction_times(trial_index):
+    return data["irt"][trial_index]
+
+
+# %%
+
+all_rows = []
+
+for s in subject_range:
+    if np.sum(subject_trial_masks[s]) == 0:
+        continue
+
+    trial_indices = jnp.where(subject_trial_masks[s])[0]
+    parameters = {
+        key: fit_result["fits"][key][s]
+        for key in fit_result["fits"]
+        if key != "subject"
+    }
+
+    # extract trial information
+    trials = likelihood_generator.trials[trial_indices]
+    accessibility = vmap(get_accessibility, in_axes=(0, None))(
+        trial_indices, parameters
+    )
+    termination_indices = vmap(get_termination_index)(trial_indices)
+    recall_positions = vmap(get_recall_positions)(trial_indices)
+    list_lengths = vmap(get_list_length)(trial_indices)
+    reaction_times = vmap(get_reaction_times)(trial_indices)
+
+    for i, trial_index in enumerate(trial_indices):
+        for j in range(termination_indices[i] + 1):
+            continuation = j < termination_indices[i]
+            rt = int(reaction_times[i][j].item())
+            log_rt = np.log(rt) if rt != 0 else 0
+            row = {
+                "subject": int(unique_subjects[s]),
+                "trial_index": int(trial_index.item()),
+                "recall": int(trials[i][j].item()),
+                "recall_position": int(recall_positions[i][j].item()),
+                "list_length": int(list_lengths[i].item()),
+                "accessibility": float(accessibility[i][j].item()),
+                "continuation": "Continued" if j < termination_indices[i].item() else "Terminated",
+                "reaction_time": rt,
+                "log_reaction_time": float(log_rt),
+            }
+            all_rows.append(row)
+
+all_rows = pd.DataFrame(all_rows)
+all_rows.head()
+
+# %%
+df = all_rows
+
+# use pivot_table to unify by subject
+# df = df.pivot_table(
+#     index=["subject", "list_length", "recall_position", "continuation"],
+#     values=["accessibility", "reaction_time"],
+#     aggfunc="mean",
+# ).reset_index()
+
+unique_list_lengths = df["list_length"].unique()
+for ll in sorted(unique_list_lengths):
+    subset = df[df["list_length"] == ll]
+
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(
+        data=subset,
+        x="recall_position",
+        y="accessibility",
+        hue="continuation",
+        errorbar="ci"
+    )
+
+    plt.xlabel("Recall Position")
+    plt.ylabel("Mean Accessibility")
+    plt.title(f"Mean Accessibility by Recall Position (List Length = {ll})")
+    handles, labels = plt.gca().get_legend_handles_labels()
+    plt.legend(title="Recall Status")
+    plt.show()
+# %%
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8, 6))
+subset = all_rows[all_rows["reaction_time"] > 0]
+
+sns.regplot(data=subset, x="accessibility", y="reaction_time", scatter_kws={'s': 10})
+plt.xlabel("Mean Accessibility")
+plt.ylabel("Reaction Time")
+plt.title("Relationship between Accessibility and Reaction Time")
+plt.show()
+
+# %%
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+subset = all_rows[all_rows["reaction_time"] > 0]
+
+plt.figure(figsize=(8, 6))
+sns.regplot(data=subset, x="accessibility", y="log_reaction_time", scatter_kws={'s': 10})
+plt.xlabel("Mean Accessibility")
+plt.ylabel("Log(Reaction Time)")
+plt.title("Relationship between Accessibility and Log(Reaction Time)")
+plt.show()
+
+# %%
+# Bin recall positions into 5 groups.
+all_rows['recall_bin'] = pd.cut(all_rows['recall_position'], bins=5)
+subset = all_rows[all_rows["reaction_time"] > 0]
+
+unique_list_lengths = subset["list_length"].unique()
+for ll in sorted(unique_list_lengths):
+    ll_subset = subset[subset["list_length"] == ll]
+    sns.lmplot(data=ll_subset, x="accessibility", y="reaction_time", hue="recall_bin", ci=95, scatter_kws={'s':10})
+    plt.xlabel("Mean Accessibility")
+    plt.ylabel("Reaction Time")
+    plt.title("Relationship between Accessibility and Reaction Time\n(Controlled by Recall Position Bin)")
+    plt.show()
+
+# %%
+# Bin recall positions into 5 groups.
+all_rows['recall_bin'] = pd.cut(all_rows['recall_position'], bins=5)
+subset = all_rows[all_rows["reaction_time"] > 0]
+
+unique_list_lengths = subset["list_length"].unique()
+for ll in sorted(unique_list_lengths):
+    ll_subset = subset[subset["list_length"] == ll]
+    sns.lmplot(data=ll_subset, x="accessibility", y="log_reaction_time", hue="recall_bin", ci=95, scatter_kws={'s':10})
+    plt.xlabel("Mean Accessibility")
+    plt.ylabel("Log(Reaction Time)")
+    plt.title("Relationship between Accessibility and Log(Reaction Time)\n(Controlled by Recall Position Bin)")
+    plt.show()
+
+# %%
+
+# %%
---
+++ projects/cmrt/demo.py
@@ -0,0 +1,259 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %%
+
+#!%load_ext autoreload
+#!%autoreload 2
+
+import json
+import os
+import warnings
+from typing import Optional
+
+import h5py
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import numpy as np
+from jax import random
+from matplotlib import rcParams  # type: ignore
+
+from cmrt.cmr import CMRFactory as model_factory
+from cmrt.fitting import ScipyDE as fitting_method
+from cmrt.helpers import import_from_string
+from cmrt.experimental.array import to_numba_typed_dict
+from cmrt.likelihood import MemorySearchLikelihoodFnGenerator as loss_fn_generator
+from cmrt.simulation import simulate_h5_from_h5
+from cmrt.summarize import summarize_parameters
+from cmrt.typing import Array, Bool
+
+warnings.filterwarnings("ignore")
+
+# %%
+
+comparison_analysis_paths = [
+    # "compmempy.analyses.rpl.plot_spacing",
+    "compmempy.analyses.spc.plot_spc",
+    "compmempy.analyses.crp.plot_crp",
+    "compmempy.analyses.pfr.plot_pfr",
+    # "compmempy.analyses.distance_crp.plot_distance_crp",
+]
+
+comparison_analyses = [import_from_string(path) for path in comparison_analysis_paths]
+
+# %%
+
+# data params
+data_name = "HerrKaha24"
+data_query = "data['listLength'] <= 40"
+data_path = "data/HerrKaha24.h5"
+run_tag = "full"
+
+# fitting params
+redo_fits = False
+model_name = "BaseCMR"
+relative_tolerance = 0.001
+popsize = 15
+num_steps = 1000
+cross_rate = 0.9
+diff_w = 0.85
+best_of = 1
+
+# sim params
+experiment_count = 10
+seed = 0
+
+parameters = {
+    "fixed": {
+    },
+    "free": {
+        "encoding_drift_rate": [2.220446049250313e-16, 0.9999999999999998],
+        "start_drift_rate": [2.220446049250313e-16, 0.9999999999999998],
+        "recall_drift_rate": [2.220446049250313e-16, 0.9999999999999998],
+        "shared_support": [2.220446049250313e-16, 99.9999999999999998],
+        "item_support": [2.220446049250313e-16, 99.9999999999999998],
+        "learning_rate": [2.220446049250313e-16, 0.9999999999999998],
+        "primacy_scale": [2.220446049250313e-16, 99.9999999999999998],
+        "primacy_decay": [2.220446049250313e-16, 99.9999999999999998],
+        "stop_probability_scale": [2.220446049250313e-16, 0.9999999999999998],
+        "stop_probability_growth": [2.220446049250313e-16, 9.9999999999999998],
+        "choice_sensitivity": [2.220446049250313e-16, 99.9999999999999998],
+    },
+}
+
+
+query_parameters = [
+    "encoding_drift_rate",
+    "start_drift_rate",
+    "recall_drift_rate",
+    "shared_support",
+    "item_support",
+    "learning_rate",
+    "primacy_scale",
+    "primacy_decay",
+    "mcf_trace_sensitivity",
+    "stop_probability_scale",
+    "stop_probability_growth",
+    "choice_sensitivity",
+]
+
+
+# %%
+def load_data(data_path: str) -> dict[str, jnp.ndarray]:
+    """Load data from hdf5 file."""
+    with h5py.File(data_path, "r") as f:
+        result = {key: f["/data"][key][()].T for key in f["/data"].keys()}  # type: ignore
+    return {key: jnp.array(value) for key, value in result.items()}
+
+
+def generate_trial_mask(
+    data: dict, trial_query: Optional[str]
+) -> Bool[Array, " trial_count"]:
+    """Returns a boolean mask for selecting trials based on a specified query condition.
+
+    Args:
+        data: dict containing trial data arrays, including a "recalls" key with an array.
+        trial_query: condition to evaluate, which should return a boolean array.
+        If None, returns a mask that selects all trials.
+    """
+    if trial_query is None:
+        return jnp.ones(data["recalls"].shape[0], dtype=bool)
+    return eval(trial_query).flatten()
+
+
+# add subdirectories for each product type: json, figures, h5
+product_dirs = {}
+for product in ["fits", "figures", "simulations"]:
+    product_dir = os.path.join(product)
+    product_dirs[product] = product_dir
+    if not os.path.exists(product_dir):
+        os.makedirs(product_dir)
+
+data = load_data(data_path)
+trial_mask = generate_trial_mask(data, data_query)
+
+max_size = np.max(data["pres_itemnos"])
+connections = jnp.zeros((max_size, max_size))
+
+# %%
+
+fit_path = os.path.join(
+    product_dirs["fits"], f"{data_name}_{model_name}_{run_tag}.json"
+)
+print(fit_path)
+
+if os.path.exists(fit_path) and not redo_fits:
+    with open(fit_path) as f:
+        results = json.load(f)
+        if "subject" not in results["fits"]:
+            results["fits"]["subject"] = results["subject"]
+
+else:
+    base_params = parameters["fixed"]
+    bounds = parameters["free"]
+    fitter = fitting_method(
+        {key: jnp.array(value) for key, value in data.items()},
+        connections,
+        base_params,
+        model_factory,
+        loss_fn_generator,
+        hyperparams={
+            "num_steps": num_steps,
+            "pop_size": popsize,
+            "relative_tolerance": relative_tolerance,
+            "cross_over_rate": cross_rate,
+            "diff_w": diff_w,
+            "progress_bar": True,
+            "display_iterations": False,
+            "bounds": bounds,
+            "best_of": best_of,
+        },
+    )
+
+    results = fitter.fit(trial_mask)
+    results = dict(results)
+
+    with open(fit_path, "w") as f:
+        json.dump(results, f, indent=4)
+
+results["data_query"] = data_query
+results["model"] = model_name
+results["name"] = f"{data_name}_{model_name}_{run_tag}"
+results["relative_tolerance"] = relative_tolerance
+results["popsize"] = popsize
+results["num_steps"] = num_steps
+results["cross_rate"] = cross_rate
+results["diff_w"] = diff_w
+
+with open(fit_path, "w") as f:
+    json.dump(results, f, indent=4)
+
+print(
+    summarize_parameters([results], query_parameters, include_std=True, include_ci=True)
+)
+
+# %%
+
+rng = random.PRNGKey(seed)
+rng, rng_iter = random.split(rng)
+sim = simulate_h5_from_h5(
+    model_factory=model_factory,
+    dataset=data,
+    connections=connections,
+    parameters={key: jnp.array(val) for key, val in results["fits"].items()},  # type: ignore
+    trial_mask=trial_mask,
+    experiment_count=experiment_count,
+    rng=rng_iter,
+)
+
+# %%
+
+unique_list_lengths = np.unique(data['listLength'][trial_mask])
+
+for LL in unique_list_lengths:
+
+    for analysis in comparison_analyses:
+        figure_str = f"{results['name']}_{LL}_{analysis.__name__[5:]}.pdf"
+        figure_path = os.path.join("figures/", figure_str)
+        print(figure_str)
+        color_cycle = [each["color"] for each in rcParams["axes.prop_cycle"]]
+        ll_trial_mask = generate_trial_mask(data, f"data['listLength'] == {LL}")
+        joint_trial_mask = np.logical_and(trial_mask, ll_trial_mask)
+        _trial_mask = generate_trial_mask(sim, data_query)
+        _ll_trial_mask = generate_trial_mask(sim, f"data['listLength'] == {LL}")
+        _joint_trial_mask = np.logical_and(_trial_mask, _ll_trial_mask)
+
+        axis = analysis(
+            datasets=[
+                to_numba_typed_dict({key: np.array(val) for key, val in sim.items()}),
+                to_numba_typed_dict({key: np.array(value) for key, value in data.items()}),
+            ],
+            trial_masks=[np.array(_joint_trial_mask), np.array(joint_trial_mask)],
+            color_cycle=color_cycle,
+            labels=["Model", "Data"],
+            contrast_name="source",
+            axis=None,
+            distances=1 - connections,
+        )
+
+        axis.tick_params(labelsize=14)
+        axis.set_xlabel(axis.get_xlabel(), fontsize=16)
+        axis.set_ylabel(axis.get_ylabel(), fontsize=16)
+        # axis.set_title(f'{results["name"]}'.replace("_", " "))
+        plt.savefig(figure_path, bbox_inches="tight")
+        plt.show()
+
+# %%
