da42a5efec4ddca155d07ebbb0e037a5b05ce489
Jordan Gunn <gunnjordanb@gmail.com>
2025-03-21 10:12:37 -0500

    Add initial project structure and versioning information

---
+++ projects/MurdockOkada1970/00_Data_Preparation.py
@@ -0,0 +1,19 @@
+# %% [markdown]
+# ---
+# title: MurdockOkada1970 Dataset
+# ---
+
+# %% [markdown]
+"""
+> Murdock, B. B., & Okada, R. (1970). Interresponse times in single-trial free recall. Journal of Experimental Psychology, 86(2), 263.  
+
+Authors investigated interresponse times in single-trial free recall. Each of 72 undergraduates was given 20 test lists with 20-word lists visually presented at either 60 or 120 words/min. The format of the data in these files is as follows:  
+
+Row 1: subject/trial information. Trial index, subject index, subject-trial index. I'm not seeing presentation speed.  
+Row 2: serial position as a function of output position.  
+Row 3: inter-response time as a function of output position  
+
+The code 88 means that the subject made an extra-list intrusion.
+"""
+
+# %%
